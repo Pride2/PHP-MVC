3d6ec8d02d00fcd1574a472169add15b9dfb48f9
Paulo Aquino <paulo041299@gmail.com>
2019-04-20 10:09:31 -0300

    V1.3

--- controller/Controller.php
+++ controller/Controller.php
@@ -1,15 +1,13 @@
 <?php 
 
 require "model/Model.php";
-require "view/View.php";
 
 class Controller
 {
     public function Index()
     {
         $model = new Model();
-        $view = new View();
-
-        $view->Exibir($model->ListarDados());
+        $dados = $model->ListarDados();
+        include "view/View.php";
     }
 }
--- view/View.php
+++ view/View.php
@@ -1,10 +1,20 @@
-<?php
-
- class View
- {
-    public function Exibir($dados)
-    {
-       echo "<h1>Exibindo a View</h1>";
-       echo "<h2>Dados: ".$dados."</h2>";
-    }
- }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP MVC</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/3.3.7/css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container">
+        <div class="jumbotron">
+             <h1>Exibindo os dados</h1>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h2>Dados: <?php echo $dados; ?></h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
